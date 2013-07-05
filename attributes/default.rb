e799e93c083424095092b272c8e1b1463acdc0d7
Josue Abreu <josue.abreu@gmail.com>
2013-07-05 17:45:46 -0400

    Latest ruby stable is 448

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default["rackbox"]["ruby"]["versions"] = %w(1.9.3-p385)
-default["rackbox"]["ruby"]["global_version"] = "1.9.3-p385"
+default["rackbox"]["ruby"]["versions"] = %w(1.9.3-p448)
+default["rackbox"]["ruby"]["global_version"] = "1.9.3-p448"
 default["rackbox"]["upstream_start_port"]["unicorn"] = 10001
 default["rackbox"]["upstream_start_port"]["passenger"] = 20001
 
