7d9d85096df83238f991f74c747c51fbd889d290
Josue Abreu <josue.abreu@gmail.com>
2013-07-05 17:41:54 -0400

    Add the mysql client gem

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@ include_recipe "appbox"
 include_recipe "rackbox::ruby"
 include_recipe "rackbox::nginx"
 include_recipe "runit"
+include_recipe "rackbox::mysql-client"
+
 
 if node["rackbox"]["apps"]["unicorn"].any?
   include_recipe "rackbox::unicorn"
