ec1b8df23c9a0d99b190920626814a5c4b380f5a
John Grayson <ajgrayson@gmail.com>
2013-12-01 15:22:01 +1300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -15,7 +15,7 @@ recipe "rackbox::passenger", "setup `passenger` apps, if any."
 supports 'ubuntu'
 supports 'debian'
 
-depends 'appbox'
+depends 'appbox', path: 'git://github.com:ajgrayson/appbox-cookbook.git'
 depends 'rbenv'
 depends 'nginx'
 depends 'unicorn', ">= 1.2.2"
