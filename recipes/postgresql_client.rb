c7a92421abed4423a82dbee38fe83f4b693de7cc
Luis Urraca <l.urraca@gmail.com>
2014-04-10 13:35:24 -0400

    Commenting out a posgres lib

--- recipes/postgresql_client.rb
+++ recipes/postgresql_client.rb
@@ -5,5 +5,5 @@
 # Install the postresql-client  if you're going to connect to a database
 #
 package 'postgresql-libs'
-package 'libpqxx-devel'
+# package 'libpqxx-devel'
 package 'postgresql-devel'
